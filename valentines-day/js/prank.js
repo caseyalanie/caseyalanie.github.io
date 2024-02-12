513377220b55398b0230b57205c1d9db6a701c1f
Casey Morales <caseyalanie@gmail.com>
2024-02-12 11:18:15 -0500

    jss completed

---
+++ valentines-day/js/prank.js
@@ -0,0 +1,19 @@
+let noButton = document.getElementById("no");
+let yesButton = document.getElementById("yes");
+let page = document.getElementById("page");
+
+let currentPadding = 24;
+
+function onNoButtonClick() {
+    currentPadding += 12;
+    yesButton.style.fontSize = currentPadding + "px";
+    yesButton.style.padding =
+        `${currentPadding}px ${currentPadding*2}px`;
+}
+
+noButton.onclick = onNoButtonClick;
+
+yesButton.onclick = () => {
+    page.innerHTML =
+        "<img id='gif' src='images/jjk.webp'>";
+}
